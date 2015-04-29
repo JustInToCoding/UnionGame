1ff3aee0bff7788b8970bc0cd551ecc1b17b3fe4
Justin Steenhuis <justinsteenhuis@gmail.com>
2015-04-29 11:24:46 +0200

    Basic pickup classes

---
+++ Source/UnionGame/Pickup.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnionGame.h"
+#include "Pickup.h"
+
+
+// Sets default values
+APickup::APickup(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	bIsActive = true;
+
+	BaseCollisionComponent = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("BaseSphereComponent"));
+
+	RootComponent = BaseCollisionComponent;
+
+	//Create the static mesh component
+	PickupMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("PickupMesh"));
+
+	//PickupMesh->SetSimulatePhysics(true);
+
+	//attach the static mesh component to the root component
+	PickupMesh->AttachTo(RootComponent);
+	
+	PickupMesh->SetCollisionProfileName("OverlapAll");
+
+	TScriptDelegate<FWeakObjectPtr> delegate = TScriptDelegate<FWeakObjectPtr>();
+
+	delegate.BindUFunction(this, "OnOverlapBegin");
+
+	PickupMesh->OnComponentBeginOverlap.Add(delegate);
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APickup::OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Destroy();
+}
+
+void APickup::OnPickedUp_Implementation()   //<--this is a Blueprint Native event, so we need to add _Implementation
+{
+	Destroy();
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+	NewLocation.Z += DeltaHeight * 20.0f;       //Scale our height by a factor of 20
+	RunningTime += DeltaTime;
+	SetActorLocation(NewLocation);
+}
---
+++ Source/UnionGame/Pickup.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class UNIONGAME_API APickup : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+		bool bIsActive;
+
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Pickup")
+	class USphereComponent* BaseCollisionComponent;
+
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Pickup")
+	class UStaticMeshComponent* PickupMesh;
+
+
+	UFUNCTION(BlueprintNativeEvent)
+		void OnPickedUp();
+
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	/** called when something enters the sphere component */
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	
+	float RunningTime;
+};
