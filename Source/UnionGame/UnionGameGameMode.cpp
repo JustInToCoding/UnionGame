d07b1a53891ea31ce1d0ead1122747eabff5c9ee
DigidragonZX <DigidragonZX@hotmail.com>
2015-05-19 11:26:10 +0200

    Gamemode so that the right character is loaded

--- Source/UnionGame/UnionGameGameMode.cpp
+++ Source/UnionGame/UnionGameGameMode.cpp
@@ -8,7 +8,7 @@ AUnionGameGameMode::AUnionGameGameMode(const FObjectInitializer& ObjectInitializ
 	: Super(ObjectInitializer)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/MyCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
