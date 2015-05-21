2bf32301aabdaa294e69a5f5e88f5e6d7991cfbe
Jochen Morent <jochen.morent@web.de>
2015-05-21 10:33:26 +0200

    Quest Backend

--- Source/UnionGame/System/QuestSystem/Quest/Quest.h
+++ Source/UnionGame/System/QuestSystem/Quest/Quest.h
@@ -3,17 +3,74 @@
 #pragma once
 
 #include "Object.h"
-#include "Quest.generated.h"
+#include "QuestTask.h"
+#include "BlueprintQuest.h"
+#include "../../../DynamicData/DDObject.h"
+
+class QuestState;
 
 /**
  * 
  */
-UCLASS()
-class UNIONGAME_API UQuest : public UObject
+class UNIONGAME_API Quest : public DDObject
 {
-	GENERATED_BODY()
-	
-	
-	
-	
+	friend TSubclassOf<QuestState>;
+
+private:
+	FString _id;
+	TArray<FString> _eventIDs;
+	bool _redoable;
+	TArray<QuestTask*> _tasks;
+	TArray<QuestTask*> _failstates;
+	TArray<FString> _startingMessages;
+	TArray<FString> _runningMessages;
+	TArray<FString> _completedMessages;
+	TArray<FString> _closedSuccessfulMessages;
+	TArray<FString> _closedFailedMessages;
+
+	QuestState* _currentState;
+	UBlueprintQuest* _wrapper;
+
+public:
+	Quest();
+	~Quest();
+
+	UBlueprintQuest* getBlueprint() { return _wrapper; }
+
+	void activate();
+	void trigger();
+	void updateTask(FString id);
+	void testState();
+	TArray<FString> getMessages();
+
+//-----------------------------------------------------------------------------------
+//  Setter
+//-----------------------------------------------------------------------------------
+	void setID(FString id);
+	void addEventID(FString eventID);
+	void removeEventID();
+	void setRedoable(bool redoaboe);
+	void addTask(QuestTask* taskID);
+	void addFailstate(QuestTask* failID);
+	void setStartingMsg(TArray<FString> msg);
+	void setRunningMsg(TArray<FString> msg);
+	void setCompletedMsg(TArray<FString> msg);
+	void setClosedSuccessfulMsg(TArray<FString> msg);
+	void setClosedFailedMsg(TArray<FString> msg);
+	void setCurrentState(QuestState* newState);
+
+//-----------------------------------------------------------------------------------
+//  Getter
+//-----------------------------------------------------------------------------------
+	FString getID() { return _id; }
+	TArray<FString> getEventIDs() { return _eventIDs; }
+	bool isRedoable() { return _redoable; }
+	EQuestTypeEnum getCurrentState();
+	TArray<QuestTask*> getTasks() { return _tasks; }
+	TArray<QuestTask*> getFailstates() { return _failstates; }
+	TArray<FString> getStartingMessages() { return _startingMessages; }
+	TArray<FString> getRunningMessages() { return _runningMessages; }
+	TArray<FString> getCompletedMessages() { return _completedMessages; }
+	TArray<FString> getClosedSuccessfulMessages() { return _closedSuccessfulMessages; }
+	TArray<FString> getClosedFailedMessages() { return _closedFailedMessages; }
 };
--- Source/UnionGame/System/QuestSystem/Quest/QuestTask.h
+++ Source/UnionGame/System/QuestSystem/Quest/QuestTask.h
@@ -3,17 +3,57 @@
 #pragma once
 
 #include "Object.h"
-#include "QuestTask.generated.h"
+
+class QuestConverter;
+class Quest;
 
 /**
  * 
  */
-UCLASS()
-class UNIONGAME_API UQuestTask : public UObject
+class UNIONGAME_API QuestTask
 {
-	GENERATED_BODY()
-	
-	
-	
-	
+	friend class QuestConverter;
+
+protected:
+	Quest* _main;
+
+public:
+	QuestTask(Quest* main);
+	~QuestTask();
+
+	virtual void start();
+	virtual void update(FString id);
+
+	virtual bool isComplete();
+};
+
+class UNIONGAME_API QuestTask_IDTracker : public QuestTask
+{
+	friend class QuestConverter;
+
+private:
+	FString _ID;
+	int _maxCount;
+	int _currentCount;
+
+public:
+	QuestTask_IDTracker(Quest* main, FString id, int maxCount);
+	void update(FString id);
+
+	bool isComplete();
+};
+
+class UNIONGAME_API QuestTask_Timer : public QuestTask
+{
+	friend class QuestConverter;
+
+private:
+	int _time;
+	bool _finished;
+
+public:
+	QuestTask_Timer(Quest* main, int time);
+	void start();
+	bool isComplete();
+	void timerRunOut();
 };
