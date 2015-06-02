fa5f1d450b7cdf571ab7a89b1f621f687e75fe1d
Justin Steenhuis <justinsteenhuis@gmail.com>
2015-06-02 18:38:25 +0200

    Health and stamina bar now properly working.

--- Source/UnionGame/UI/MyHUD.h
+++ Source/UnionGame/UI/MyHUD.h
@@ -25,12 +25,25 @@ class AMyHUD : public AHUD
 	void DrawHUD_Reset();
 
 private:
-	float 		MaxValue;
-	float 		Value;
+	float 		maxHealth;
+	float 		health;
+	float		maxStamina;
+	float		stamina;
 
 	bool InventoryOnScreen = 0;
 
 	TSharedPtr<class SInventoryWidget> InventoryWidget;
+public:
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void setMaxHealth(float health);
 
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void setHealth(float health);
+
+	UFUNCTION(BlueprintCallable, Category = "Stamina")
+	void setMaxStamina(float stamina);
+
+	UFUNCTION(BlueprintCallable, Category = "Stamina")
+	void setStamina(float stamina);
 };
 
